6f6a1aec0b2200b12a0ac8c630169210b83a0317
sumanrakshit <sumanrakshit1997@gmail.com>
2025-12-09 22:43:39 +0530

    just modify

--- src/main/java/com/employeecicd/deployement/DeployementApplication.java
+++ src/main/java/com/employeecicd/deployement/DeployementApplication.java
@@ -8,6 +8,7 @@ public class DeployementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeployementApplication.class, args);
+		System.out.println("test");
 
 	}
 
